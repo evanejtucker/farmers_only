3fa344ecf98d104e73e058dcc31fb2ba65489b60
Evan Tucker <eej.tucker@gmail.com>
2017-08-26 12:50:57 -0600

    :grin:

--- farmazon/src/app/app.module.ts
+++ farmazon/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignInComponent,
+    SignUpComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
